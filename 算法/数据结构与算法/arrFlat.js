099b37cb53e532fdb86b0f4474c3cab7208fae13
jianyi <jianeasy@foxmail.com>
2024-03-28 00:24:42 +0800

    add: some code

--- 算法/数据结构与算法/arrFlat.js
+++ 算法/数据结构与算法/arrFlat.js
@@ -1,14 +1,14 @@
 const arrFlat = (arr) => {
-    let res = []
-    arr.forEach((item) => {
-        if(Array.isArray(item)){
-            res = res.concat(arrFlat(item));
-        } else{
-            res.push(item)
-        }
-    });
-    return res
-}
-const arr = [1, [2, [3, 4]]];
+  let res = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      res = res.concat(arrFlat(item));
+    } else {
+      res.push(item);
+    }
+  });
+  return res;
+};
+const arr = [1, [2, [3, 4, [5, 6]]]];
 
 console.log(arrFlat(arr));
